528868c2b82ac6d1040ac745e8c8d85e24814562
tientm-1087 <tran.minh.tien@sun-asterisk.com>
2022-05-25 13:12:40 +0700

    refactor

--- lib/signUpScreen.dart
+++ lib/signUpScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_homework_1/View/TopView.dart';
 
+import 'View/CustomButton.dart';
+
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
@@ -27,87 +29,32 @@ class SignUpScreen extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const CustomButton(text: "Sign in with Twitter", imageName: 'twitter'),
+            const CustomButton(
+                text: "Sign in with Twitter", imageName: 'twitter'),
             const SizedBox(
               height: 20,
             ),
             const CustomButton(text: "Sign Up", imageName: ''),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 child: const Text("ALREADY REGISTERED? SIGN IN",
                     style: TextStyle(
-                    shadows: [Shadow(color: Colors.white, offset: Offset(0, -1))],
-                    color: Colors.transparent,
-                    decorationThickness: 2,
-                    decorationColor: Colors.white,
-                    decorationStyle: TextDecorationStyle.solid,
-                    decoration: TextDecoration.underline)))
+                        shadows: [
+                          Shadow(color: Colors.white, offset: Offset(0, -1))
+                        ],
+                        color: Colors.transparent,
+                        decorationThickness: 2,
+                        decorationColor: Colors.white,
+                        decorationStyle: TextDecorationStyle.solid,
+                        decoration: TextDecoration.underline)))
           ],
         ),
       ),
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.text, required this.imageName})
-      : super(key: key);
-
-  final String text;
-  final String imageName;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 315,
-      height: 60,
-      child: ElevatedButton(
-          onPressed: () {},
-          style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ))),
-          child: Row(
-            mainAxisAlignment: imageName.isEmpty
-                ? MainAxisAlignment.center
-                : MainAxisAlignment.start,
-            children: [
-              if (!imageName.isEmpty) ...[
-                Image.asset(
-                  'assets/images/$imageName.png',
-                  width: 40,
-                  height: 50,
-                ),
-                const SizedBox(
-                  width: 1,
-                  height: 29,
-                  child: DecoratedBox(
-                      decoration: BoxDecoration(color: Colors.red)),
-                ),
-                Expanded(
-                    child: Padding(
-                  padding: EdgeInsets.only(right: 40),
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ))
-              ] else ...[
-                Text(
-                  text,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                )
-              ]
-            ],
-          )),
     );
   }
 }
